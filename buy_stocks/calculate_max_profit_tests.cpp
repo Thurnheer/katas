dd844cd4e47dcc3d8c1907fa02a9483ccc45bf26
Christoph Thurnheer <c.thurnheer@gmx.ch>
2019-04-30 21:43:14 +0200

    solved "buy stocks"

--- buy_stocks/calculate_max_profit_tests.cpp
+++ buy_stocks/calculate_max_profit_tests.cpp
@@ -4,9 +4,10 @@
 
 namespace kata {
 
-TEST(max_profit, should_)
+TEST(max_profit, should_be_0)
 {
-  FAIL();
+    std::vector<int> stock_prices{3, 2, 1};
+    ASSERT_EQ(0, calculate_max_profit(stock_prices));
 }
 
 } // namespace kata
