3ab8741a14ff174cdaa90580ce2ad7eeb326067b
Christoph Thurnheer <c.thurnheer@gmx.ch>
2019-05-01 23:02:21 +0200

    added solution of paolo's group, implemented with std accumulate

--- buy_stocks/calculate_max_profit.hpp
+++ buy_stocks/calculate_max_profit.hpp
@@ -4,10 +4,9 @@
 #include <iterator>
 #include <numeric>
 #include <vector>
+#include <tuple>
 
-#include <iostream>
-
-namespace kata {
+namespace kataSolution2 {
 
 auto min = [](auto a, auto b) {
     return a < b ? a : b;
@@ -29,11 +28,39 @@ auto adjacent_f(InputItr begin, InputItr end, Op op)
 template <typename T>
 T calculate_max_profit(const std::vector<T>& stock_prices)
 {
-    std::vector<T> minValues(adjacent_f(stock_prices.begin(), stock_prices.end(), kata::min));
-    std::vector<T> maxValues(adjacent_f(stock_prices.rbegin(), stock_prices.rend(), kata::max));
+    std::vector<T> minValues(adjacent_f(stock_prices.begin(), stock_prices.end(), kataSolution2::min));
+    std::vector<T> maxValues(adjacent_f(stock_prices.rbegin(), stock_prices.rend(), kataSolution2::max));
 
     return std::inner_product(maxValues.rbegin(), maxValues.rend(),
-                minValues.begin(), 0, kata::max, std::minus<>());
+                minValues.begin(), 0, kataSolution2::max, std::minus<>());
+}
+
+} // namespace kata
+
+
+namespace kata
+{
+
+/*auto minOfDifference = [](std::tuple<auto auto>, auto value) {*/
+template<typename T>
+std::tuple<T,T> minOfDifference(std::tuple<T, T> acc, T value) {
+    auto& [currentMinimum, difference] = acc;
+    currentMinimum = kataSolution2::min(currentMinimum, value);
+    difference = value - currentMinimum;
+    return acc;
+}
+
+template <typename T>
+T calculate_max_profit(const std::vector<T>& stock_prices)
+{
+    return std::get<1>(std::accumulate(stock_prices.begin(), stock_prices.end(),
+        std::make_tuple<T,T>(T{std::numeric_limits<T>::max()}, T{0}),
+        [](std::tuple<T,T> acc, T value){
+            auto& [currentMinimum, difference] = acc;
+            currentMinimum = kataSolution2::min(currentMinimum, value);
+            difference = kataSolution2::max(difference, value - currentMinimum);
+            return acc;
+        }));
 }
 
 } // namespace kata
