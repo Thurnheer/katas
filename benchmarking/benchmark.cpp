fc300ab2f25ea86e951a588f40a7207abef486bf
Christoph Thurnheer <christopht@christophlaptop.fritz.box>
2019-05-10 23:19:25 +0200

    beautyfied benchmarking solution

--- benchmarking/benchmark.cpp
+++ benchmarking/benchmark.cpp
@@ -3,4 +3,74 @@
 #include <benchmark/benchmark.h>
 #include <random>
 
+
+class StockTester : public ::benchmark::Fixture
+
+{
+
+public:
+
+  void SetUp(const ::benchmark::State& st) override
+
+  {
+
+    input = std::vector<double>(st.range(0));
+
+    constexpr int fixed_seed = 3;
+
+    std::mt19937  gen(fixed_seed);
+
+    std::uniform_int_distribution<> dis(1, 100);
+
+    for(auto& val : input)
+
+      val = dis(gen);
+
+  }
+
+ 
+
+ void TearDown(const ::benchmark::State&) override
+
+  {
+
+    input.clear();
+
+  }
+
+  std::vector<double> input;
+
+};
+
+ 
+
+BENCHMARK_DEFINE_F(StockTester, howFastDoWeCalculateProfit)(benchmark::State& state)
+
+{
+
+  for(auto _ : state)
+
+    benchmark::DoNotOptimize(kata::calculate_max_profit(input));
+
+  state.SetComplexityN(state.range(0));
+
+}
+
+ 
+
+constexpr unsigned int minNumberOfElements{1};
+
+constexpr unsigned int maxNumberOfElements{std::numeric_limits<int32_t>::max() / 8};
+
+ 
+
+BENCHMARK_REGISTER_F(StockTester, howFastDoWeCalculateProfit)
+
+->RangeMultiplier(2)
+
+  ->Range(minNumberOfElements, maxNumberOfElements)
+
+  ->Complexity();
+
+ 
 BENCHMARK_MAIN();
--- benchmarking/solution2/solution.hpp
+++ benchmarking/solution2/solution.hpp
@@ -6,27 +6,20 @@
 #include <vector>
 #include <tuple>
 
-namespace kataSolution2 {
-
-auto min = [](auto a, auto b) { return a < b ? a : b; };
-auto max = [](auto a, auto b) { return a > b ? a : b; };
-
-} // namespace kataSolution2
-
 namespace kata {
 
 template <typename T>
 T calculate_max_profit(const std::vector<T>& stock_prices)
 {
-  return std::get<1>(std::accumulate(stock_prices.begin(),
-                                     stock_prices.end(),
-                                     std::make_tuple<T, T>(T{ std::numeric_limits<T>::max() }, T{ 0 }),
-                                     [](std::tuple<T, T> acc, T value) {
-                                       auto& [currentMinimum, difference] = acc;
-                                       currentMinimum                     = kataSolution2::min(currentMinimum, value);
-                                       difference = kataSolution2::max(difference, value - currentMinimum);
-                                       return acc;
-                                     }));
+  return std::get<1>(
+        std::accumulate(stock_prices.begin(), stock_prices.end(),
+            std::make_tuple<T, T>(T{ std::numeric_limits<T>::max() }, T{ 0 }),
+            [](std::tuple<T, T> acc, T value) {
+                auto& [currentMinimum, difference] = acc;
+                currentMinimum                     = std::min(currentMinimum, value);
+                difference = std::max(difference, value - currentMinimum);
+                return acc;
+            }));
 }
 
 } // namespace kata
